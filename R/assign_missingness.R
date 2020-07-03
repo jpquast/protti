cccb370a64240871d097bce1bfd3e76f3cd185c0
Jan-Philipp Quast <janphilipp.quast@gmail.com>
2020-07-03 17:43:07 +0200

    Add assign_missingness

---
+++ R/assign_missingness.R
@@ -0,0 +1,62 @@
+#' Assignment of missingness types
+#'
+#' The type of missingness (missing at random, missing not at random) is assigned based on the comparison of a reference condition and every other condition.
+#'
+#' @param data a data frame containing at least the input variables.
+#' @param sample the name of the column containing the sample names.
+#' @param condition the name of the column containing the conditions.
+#' @param grouping the name of the column containing peptide, precursor or protein information depending on which level data is evaluated.
+#' @param intensity the name of the column containing intensity values.
+#' @param ref_condition the condition that is used as a reference for missingness determination. By default \code{ref_condition = "control"}.
+#'
+#' @return A data frame that contains the reference condition paired with each treatment condition. The \code{conditions} column contains the name of the treatment for the specific reference/treatment pair. The \code{missingness} column reports the type of missingness. 
+#' \itemize{
+#' \item{"complete": }{No missing values for every replicate of this reference/treatment pair for the specific grouping variable.}
+#' \item{"MNAR": }{Missing not at random. All replicates of either the reference or treatment condition have missing values for the specific grouping variable.}
+#' \item{"MAR": }{Missing at random. At least n-1 replicates have missing values for the reference/treatment pair for the specific grouping varible.}
+#' } 
+#' @import dplyr
+#' @import tidyr
+#' @importFrom rlang .data
+#' @importFrom rlang as_name
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' assign_missingness(
+#' data,
+#' sample = r_file_name,
+#' condition = r_condition,
+#' grouping = eg_precursor_id,
+#' intensity = normalised_intensity_log2
+#' )
+#' }
+assign_missingness <- function(data, sample, condition, grouping, intensity, ref_condition = "control"){
+data <-  data %>%
+    dplyr::distinct({{sample}}, {{condition}}, {{grouping}}, {{intensity}}) %>%
+    tidyr::complete(nesting(!!ensym(sample), !!ensym(condition)), !!ensym(grouping)) %>%
+    dplyr::mutate(detect = !is.na({{intensity}})) %>%
+    dplyr::group_by({{grouping}}, {{condition}}) %>%
+    dplyr::mutate(n_detect = sum(.data$detect)) %>%
+    dplyr::mutate(n_replicates = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(conditions = ifelse(
+      {{condition}} %in% unique(pull(data, !!ensym(condition)))[!unique(pull(data, !!ensym(condition))) %in% ref_condition],
+      {{condition}},
+      list(unique(pull(data, !!ensym(condition)))[!unique(pull(data, !!ensym(condition))) %in% ref_condition])
+    ))%>%
+    tidyr::unnest(.data$conditions) 
+  
+data %>%
+    dplyr::distinct({{condition}}, {{grouping}}, .data$n_detect, .data$n_replicates, .data$conditions) %>%
+    dplyr::group_by({{grouping}}, .data$conditions) %>%
+    dplyr::mutate(missingness = dplyr::case_when(.data$n_detect == .data$n_replicates & lag(.data$n_detect) == .data$n_replicates ~ "complete",
+                                   .data$n_detect == 0 & lag(.data$n_detect) == .data$n_replicates ~ "MNAR",
+                                   .data$n_detect == .data$n_replicates & lag(.data$n_detect) == 0 ~ "MNAR",
+                                   .data$n_detect >= .data$n_replicates - 1 & lag(.data$n_detect) >= .data$n_replicates -1 ~ "MAR")) %>%
+    tidyr::fill(.data$missingness, .direction = "updown") %>%
+  dplyr::select(-.data$n_detect, -.data$n_replicates) %>%
+   dplyr::left_join(data, by = c("conditions", rlang::as_name(enquo(condition)), rlang::as_name(enquo(grouping)))) %>%
+  dplyr::select(-.data$n_detect, -.data$n_replicates, -.data$detect)
+}
