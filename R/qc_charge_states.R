04a8907926d553446598c619d1327c678c81ed2e
Dina Schuster <dschuster@ethz.ch>
2020-07-06 19:58:44 +0200

    Edit roxygen header @example

--- R/qc_charge_states.R
+++ R/qc_charge_states.R
@@ -21,7 +21,7 @@
 #'
 #' @examples
 #' \dontrun{
-#' qc_charge_states(data, sample, grouping, charge_states, intensity = NULL, plot = TRUE)
+#' qc_charge_states(data, sample, grouping, charge_states, intensity = NULL, method = "count", plot = TRUE)
 #' }
 qc_charge_states <-
   function(data, sample, grouping, charge_states, intensity = NULL, method, plot = FALSE)
