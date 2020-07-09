19a2d98374c1dd1f705d0c2d354550ff973bb748
Dina Schuster <dschuster@ethz.ch>
2020-07-09 13:30:04 +0200

    Edit @examples, remove group_by, change plot title

--- R/qc_run_intensity.R
+++ R/qc_run_intensity.R
@@ -2,10 +2,10 @@
 #'
 #' Plots the intensities in each sample as a boxplot
 #'
-#' @param data A dataframe containing at least sample names, peptide or precursor identifiers and log2 transformed intensities for each peptide or precursor.
+#' @param data A dataframe containing at least sample names, peptide or precursor identifiers and (log2 transformed) intensities for each peptide or precursor.
 #' @param sample The column in the data dataframe containing the sample name.
 #' @param grouping The column in the data dataframe containing either precursor or peptide identifiers.
-#' @param log2intensity The column in the data dataframe containing the log2 transformed intensities of each precursor or peptide.
+#' @param intensity The column in the data dataframe containing the (log2 transformed) intensities of each precursor or peptide.
 #'
 #' @return A plot with one boxplot for each sample, depicting the peptide or precurosor intensities.
 #' @import dplyr
@@ -15,24 +15,24 @@
 #'
 #' @examples
 #' \dontrun{
-#' qc_run_intensity(data,
-#' sample,
-#' grouping,
-#' log2intensity)
+#' qc_run_intensity(
+#' data,
+#' sample = r_file_name,
+#' grouping = pep_stripped_sequence,
+#' intensity = normalised_intensity_log2)
 #' }
 qc_run_intensity <-
-  function(data, sample, grouping, log2intensity)
+  function(data, sample, grouping, intensity)
   {
     result <- data %>%
-      dplyr::distinct({{sample}}, {{grouping}}, {{log2intensity}}) %>%
-      dplyr::group_by({{sample}}) %>%
-      dplyr::filter(!is.na({{log2intensity}}))
+      dplyr::distinct({{sample}}, {{grouping}}, {{intensity}}) %>%
+      dplyr::filter(!is.na({{intensity}}))
 
     plot <- result %>%
-      ggplot2::ggplot(aes(x = {{sample}}, y = {{log2intensity}})) +
+      ggplot2::ggplot(aes(x = {{sample}}, y = {{intensity}})) +
       geom_boxplot(fill = "cornflowerblue", outlier.color = "orchid3") +
       theme_bw() +
-      labs(title = "Boxplots of log2 transformed peptide intensities in each sample", x = "sample", y = "log2(intensity)")
+      labs(title = "Run intensities", x = "sample", y = "intensity")
     return(plot)
   }
 
