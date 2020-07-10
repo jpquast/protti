b53a30fa92238ac6111bf621efcba0f83736e6e1
jpquast <janphilipp.quast@gmail.com>
2020-07-10 15:23:35 +0200

    Add documentation

--- R/drc_4p.R
+++ R/drc_4p.R
@@ -1,7 +1,26 @@
-
-
-drc_4p <- function(data, response, dose, pb) {
-  pb$tick()
+#' Dose response curve helper function
+#'
+#' This function peforms the four-parameter dose response curve fit. It is the helper function for the fit in the \code{fit_drc_4p} function.
+#'
+#' @param data a data frame that contains at least the dose and response column the model should be fitted to.
+#' @param response the name of the column that contains the response values.
+#' @param dose the name of the column that contains the dose values.
+#' @param pb progress bar object. This is only necessary if the function is used in an iteration. 
+#'
+#' @return An object of class \code{drc}. If no fit was performed a character vector with content "no_fit". 
+#' @importFrom drc drm
+#' @importFrom drc LL.4
+#'
+#' @examples
+#' \dontrun{
+#' drc_4p(
+#' data,
+#' response = intensity,
+#' dose = concentration
+#' )
+#' }
+drc_4p <- function(data, response, dose, pb = NULL) {
+  if (!is.null(pb)) pb$tick()
   tryCatch({
     suppressWarnings(drc::drm(
       stats::as.formula(paste(ensym(response), "~", ensym(dose))),
@@ -12,18 +31,3 @@ drc_4p <- function(data, response, dose, pb) {
     c("no_fit")
   })
 }
-
-
-# drc_4p <- function(data, response, dose, pb) {
-#   pb$tick()
-#   tryCatch({
-#     suppressWarnings(dr4pl::dr4pl(
-#       stats::as.formula(paste(ensym(response), "~", ensym(dose))),
-#       data = data,
-#       method.init = "logistic",
-#       use.Hessian = TRUE
-#       ))
-#   }, error = function(error) {
-#     c("no_fit")
-#   })
-# }
--- R/fit_drc_4p.R
+++ R/fit_drc_4p.R
@@ -1,11 +1,51 @@
+#' Fitting four-parameter dose response curves
+#'
+#' Function for fitting four-parameter dose response curves for each group (precursor, peptide or protein). 
+#'
+#' @param data a data frame containing at least the input variables.
+#' @param sample the name of the column containing the sample names.
+#' @param grouping the name of the column containing precursor, peptide or protein identifiers.
+#' @param response the name of the column containing response values, eg. log2 transformed intensities.
+#' @param dose the name of the column containing dose values, eg. the treatment concentrations.
+#' 
+#' @return A list that contains: 
+#' \itemize{
+#' \item{\code{fit_objects}: }{The fit objects of type \code{drc} for each group. }
+#' \item{\code{correlations}: }{A data frame that contains correlation of predicted to measured values as a measure of the goodness of the curve fit, an associated p-value and the four parameters of the model for each group.}
+#' \item{\code{plots}: }{The ggplot objects containing the curve plot. Printing these will print the plot.}
+#' } 
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @import progress
+#' @import ggplot2
+#' @importFrom tibble tibble
+#' @importFrom tibble as_tibble
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fit_drc_4p(
+#' data,
+#' sample = r_file_name,
+#' grouping = eg_precursor_id,
+#' response = intensity,
+#' dose = concentration
+#' )
+#' }
 fit_drc_4p <- function(data, sample, grouping, response, dose){
+  # to prevent no visible binding for global variable '.' note.
+  . = NULL
+  
   input <- data %>%
     dplyr::distinct({{sample}}, {{grouping}}, {{response}}, {{dose}}) %>%
     tidyr::drop_na({{response}}) %>%
     dplyr::group_by({{grouping}}) %>%
-    dplyr::mutate(n_concentrations = n_distinct(!!ensym(dose))) %>%
+    dplyr::mutate(n_concentrations = dplyr::n_distinct(!!ensym(dose))) %>%
     dplyr::ungroup() %>%
-    split(., pull(., !!ensym(grouping))) %>%
+    split(., dplyr::pull(., !!ensym(grouping))) %>%
     purrr::keep( ~ unique(.x$n_concentrations) > 4) 
   
   pb <- progress::progress_bar$new(total = length(input), format = "  Model fitting [:bar] :percent :eta")
@@ -14,56 +54,56 @@ fit_drc_4p <- function(data, sample, grouping, response, dose){
   
   pb <- progress::progress_bar$new(total = length(input), format = "  1/4 Calculating correlations [:bar] :percent :eta")
   correlation_output <- input%>%
-    map2(fit_objects, function(x, y) {
+    purrr::map2(fit_objects, function(x, y) {
       pb$tick()
       tryCatch({
-        suppressWarnings(cor(pull(x, !!ensym(response)), stats::fitted(y), method = "pearson"))
+        suppressWarnings(stats::cor(pull(x, !!ensym(response)), stats::fitted(y), method = "pearson"))
       }, error = function(error){
         c("no_correlation")
       })
     })%>%
-    keep(~ . != "no_correlation") %>%
-    map( ~ tibble(correlation = .)) %>%
-    map2_dfr(names(.), function(x, y)
-      mutate(x, sequence = y))
+    purrr::keep(~ . != "no_correlation") %>%
+    purrr::map( ~ tibble::tibble(correlation = .)) %>%
+    purrr::map2_dfr(names(.), function(x, y)
+      dplyr::mutate(x, sequence = y))
   
   pb <- progress::progress_bar$new(total = length(input), format = "  2/4 Calculating p-values [:bar] :percent :eta")
   p_value_correlation <- input%>%
-    map2(fit_objects, function(x, y) {
+    purrr::map2(fit_objects, function(x, y) {
       pb$tick()
       tryCatch({
-        suppressWarnings(cor.test(pull(x, !!ensym(response)), fitted(y), method = "pearson")$p.value)
+        suppressWarnings(stats::cor.test(dplyr::pull(x, !!ensym(response)), stats::fitted(y), method = "pearson")$p.value)
       }, error = function(error) {
         c("no_pvalue")
       })
     })%>%
-    keep(~ . != "no_pvalue") %>%
-    map( ~ tibble(p_value = .)) %>%
-    map2_dfr(names(.), function(x, y)
-      mutate(x, sequence = y))
+    purrr::keep(~ . != "no_pvalue") %>%
+    purrr::map( ~ tibble::tibble(p_value = .)) %>%
+    purrr::map2_dfr(names(.), function(x, y)
+      dplyr::mutate(x, sequence = y))
   
   predictions_range <- input %>%
-    purrr::map(~ filter(., {{dose}} != 0)) %>%
-    purrr::map( ~ tibble(min = min(pull(.,!!ensym(dose))), max = max(pull(.,!!ensym(dose))))) %>%
+    purrr::map(~ dplyr::filter(., {{dose}} != 0)) %>%
+    purrr::map( ~ tibble::tibble(min = min(pull(.,!!ensym(dose))), max = max(pull(.,!!ensym(dose))))) %>%
     purrr::map( ~ expand.grid(dose = exp(seq(log(.$max), log(.$min), length = 100))))
   
   pb <- progress::progress_bar$new(total = length(fit_objects), format = "  3/4 Predicting curves for plots [:bar] :percent :eta")
   predictions <- fit_objects %>%
     purrr::map2(predictions_range, function(x, y){
       pb$tick()
-      tryCatch({suppressWarnings(predict(x, newdata = y, interval = "confidence"))},
+      tryCatch({suppressWarnings(stats::predict(x, newdata = y, interval = "confidence"))},
                error = function(error){
-                 tibble(Prediction = "no_fit")
+                 tibble::tibble(Prediction = "no_fit")
                })
     }) %>%
-    purrr::map(~ as_tibble(.x))
+    purrr::map(~ tibble::as_tibble(.x))
   
   line_fit <- predictions_range %>%
-    purrr::map(~ as_tibble(.x)) %>%
+    purrr::map(~ tibble::as_tibble(.x)) %>%
     purrr::map2(predictions, function(x, y){
       bind_cols(x, y)
     }) %>%
-    keep(~ !("no_fit" %in% unique(.x$Prediction)) )
+    purrr::keep(~ !("no_fit" %in% unique(.x$Prediction)) )
   
   names_to_keep <- names(line_fit)
   
@@ -72,26 +112,26 @@ fit_drc_4p <- function(data, sample, grouping, response, dose){
     .[names_to_keep] %>%
     purrr::map2(line_fit, function(x, y){ 
       pb$tick()
-      ggplot(data = x, aes(x = {{dose}}, y = {{response}})) +
-        geom_point() +
-        suppressWarnings(geom_ribbon(data = y, aes(x = dose, y = Prediction, ymin = Lower, ymax = Upper), alpha = 0.2)) +
-        geom_line(data = y, aes(x=dose, y = Prediction)) +
-        labs(title = names(.)) +
-        theme_bw()+
-        theme(axis.text.x = element_text(angle = 45, hjust =1)) +
-        scale_x_log10() 
+      ggplot2::ggplot(data = x, ggplot2::aes(x = {{dose}}, y = {{response}})) +
+        ggplot2::geom_point() +
+        suppressWarnings(ggplot2::geom_ribbon(data = y, ggplot2::aes(x = dose, y = .data$Prediction, ymin = .data$Lower, ymax = .data$Upper), alpha = 0.2)) +
+        ggplot2::geom_line(data = y, ggplot2::aes(x=dose, y = .data$Prediction)) +
+        ggplot2::labs(title = names(.)) +
+        ggplot2::theme_bw()+
+        ggplot2::theme(axis.text.x = element_text(angle = 45, hjust =1)) +
+        ggplot2::scale_x_log10() 
       }
     )
   
   correlation_output <- correlation_output %>%
-    left_join(p_value_correlation, by = "sequence")
+    dplyr::left_join(p_value_correlation, by = "sequence")
   
   correlation_output <- fit_objects %>%
-    map(function(x) {
-      bind_rows(pluck(x, "coefficients"))}) %>%
-    map2_dfr(names(.), function(x, y)
-      mutate(x, sequence = y)) %>%
-    left_join(correlation_output, by = "sequence")
+    purrr::map(function(x) {
+      dplyr::bind_rows(purrr::pluck(x, "coefficients"))}) %>%
+    purrr::map2_dfr(names(.), function(x, y)
+      dplyr::mutate(x, sequence = y)) %>%
+    dplyr::left_join(correlation_output, by = "sequence")
   
   result <- list(fit_objects = fit_objects, correlations = correlation_output, plots = plots)
   result
