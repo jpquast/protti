de0e36b11b19c1dfd00b40bb2915f666e467d3ea
jpquast <janphilipp.quast@gmail.com>
2020-07-20 09:53:33 +0200

    Fix typos and change argument name

--- R/qc_sample_correlation.R
+++ R/qc_sample_correlation.R
@@ -7,7 +7,7 @@
 #' @param grouping the name of the column containing precursor or peptide identifiers.
 #' @param intensity the name of the column containing intensity values. Note: The input intensities should be log2 transformed.
 #' @param condition the name of the column containing the conditions.
-#' @param pipeline optional, the name of the column containing information about the pipline used. Eg. "LiP" or "tryptic control".
+#' @param digestion optional, the name of the column containing information about the digestion method used. Eg. "LiP" or "tryptic control".
 #' @param run_order optional, the name of the column containing the order in which samples were measured. Useful to investigate batch effects due to run order.
 #' @param method the method to be used for correlation. \code{"spearman"} is the default but can be changed to \code{"pearson"} or \code{"kendall"}.
 #' @param interactive logical, default is \code{TRUE}. Determines if an interactive or static heatmap should be created using \code{heatmaply} or \code{pheatmap}, respectively.
@@ -37,7 +37,7 @@
 #' condition = r_condition
 #' )
 #' }
-qc_sample_correlation <- function(data, sample, grouping, intensity, condition, pipeline = NULL, run_order = NULL, method = "spearman", interactive = TRUE){
+qc_sample_correlation <- function(data, sample, grouping, intensity, condition, digestion = NULL, run_order = NULL, method = "spearman", interactive = TRUE){
   correlation <- data %>%
     dplyr::distinct({{sample}}, {{grouping}}, {{intensity}}) %>%
     tidyr::pivot_wider(names_from = {{sample}}, values_from = {{intensity}}) %>%
@@ -45,13 +45,13 @@ qc_sample_correlation <- function(data, sample, grouping, intensity, condition, 
     stats::cor(method = {{method}}, use = "complete.obs") 
   
   annotation <- data %>%
-    dplyr::distinct({{sample}}, {{condition}}, {{pipeline}}, {{run_order}}) %>%
+    dplyr::distinct({{sample}}, {{condition}}, {{digestion}}, {{run_order}}) %>%
     tibble::column_to_rownames(var = rlang::as_name(enquo(sample)))
   
   # Create heatmaply dendrogram for pheatmap
   distance <- stats::dist(correlation)
-  hirachical_clustering <- stats::hclust(distance)
-  dendrogram <- stats::as.dendrogram(hirachical_clustering)
+  hierachical_clustering <- stats::hclust(distance)
+  dendrogram <- stats::as.dendrogram(hierachical_clustering)
   dendrogram_row <- dendextend::seriate_dendrogram(dendrogram, distance, method="OLO")
   dendrogram_column <- dendextend::rotate(dendrogram_row, order = rev(labels(distance)[seriation::get_order(stats::as.hclust(dendrogram_row))]))
   
