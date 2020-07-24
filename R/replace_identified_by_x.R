ac11c5073778fdbb1e71261003bd9894bbe7a57a
Dina Schuster <dschuster@ethz.ch>
2020-07-24 18:08:38 +0200

    Add replace_identified_by_x (helper function for sequence coverage calculation)

---
+++ R/replace_identified_by_x.R
@@ -0,0 +1,34 @@
+#' Replace each identified positions in protein by "x"
+#'
+#' Helper function for the calculation of sequence coverage, replaces identified positions with an "x"
+#'
+#' @param sequence Protein sequence
+#' @param positions_start Start position of the identified peptide.
+#' @param positions_end End position of the identified peptide.
+#'
+#' @return A plotted principal component analysis showing PC1 and PC2
+#' @import dplyr
+#' @import purrr
+#' @import tidyr
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_sub
+#'
+#' @examples
+#' \dontrun{
+#' replace_identified_by_x(
+#' sequence = protein_sequence,
+#' positions_start = start,
+#' positions_end = end
+#' )
+#' }
+#'
+replace_identified_by_x <- function(sequence, positions_start, positions_end)
+  {
+  result <- purrr::map2(.x = positions_start, .y = positions_end,
+                        function(x, y){
+    times <- y-x + 1
+    stringr::str_sub(sequence, start = x, end = y) <- paste(rep("x", times = times), collapse = "")
+    sequence <<- sequence
+  })
+  result[[length(result)]]
+}
