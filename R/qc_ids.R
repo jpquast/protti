e68fbe7f00e5970eee4e3ac8ea0bbc421270a664
Dina Schuster <dschuster@ethz.ch>
2020-07-27 15:07:15 +0200

    Add interactive argument

--- R/qc_ids.R
+++ R/qc_ids.R
@@ -8,6 +8,7 @@
 #' @param condition Optional column in the dataframe specifying the condition of the sample (e.g. LiP_treated, LiP_untreated), if column is provided, the bars in the plot will be coloured according to the condition.
 #' @param title Optional argument specifying the plot title (default is "ID count per sample").
 #' @param plot Argument specifying whether the output of the function should be plotted (default is TRUE).
+#' @param interactive Argument specifying whether the plot should be interactive (default is TRUE).
 #'
 #' @return A bar plot with the height corresponding to the number of IDs, each bar represents one sample (if \code{plot = TRUE}). If \code{plot = FALSE} a table with ID counts is returned.
 #' @import dplyr
@@ -27,17 +28,17 @@
 #' )
 #' }
 qc_ids <-
-  function(data, sample, grouping, condition = NULL, title = "ID count per sample", plot = TRUE)
+  function(data, sample, grouping, condition = NULL, title = "ID count per sample", plot = TRUE, interactive = TRUE)
   {
-    if (plot == TRUE)
-    {
-    result <- data %>%
+   result <- data %>%
       dplyr::distinct({{sample}}, {{grouping}}, {{condition}}) %>%
       dplyr::group_by({{sample}}) %>%
       dplyr::mutate(count = n()) %>%
       dplyr::select(-{{grouping}}) %>%
       dplyr::distinct()
 
+ if (plot == TRUE)
+    {
     plot <- result %>%
       ggplot2::ggplot(aes(x = {{sample}}, y = .data$count, fill = {{condition}})) +
       geom_col(col = "black") +
@@ -47,14 +48,13 @@ qc_ids <-
            y = "count") +
       theme_bw() +
       theme(axis.text.x = element_text(angle = 75, hjust = 1))
-    return(plotly::ggplotly(plot))
+    if (interactive == TRUE)
+    {
+      return(plotly::ggplotly(plot))
     } else {
-      result <- data %>%
-        dplyr::distinct({{sample}}, {{grouping}}, {{condition}}) %>%
-        dplyr::group_by({{sample}}) %>%
-        dplyr::mutate(ID_count = n()) %>%
-        dplyr::select(-{{grouping}}) %>%
-        dplyr::distinct()
+      return(plot)
+    }
+    } else {
       return(result)
     }
   }
