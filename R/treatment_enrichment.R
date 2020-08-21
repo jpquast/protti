5fa083dae79fcac5252122e33c27052a5eab1d1a
jpquast <janphilipp.quast@gmail.com>
2020-08-21 12:44:50 +0200

    Add treatment_enrichment function

---
+++ R/treatment_enrichment.R
@@ -0,0 +1,66 @@
+treatment_enrichment <- function(data, protein_id, is_significant, binds_treatment, treatment_name, plot = TRUE){
+  data <- data %>%
+    dplyr::distinct({{protein_id}}, {{is_significant}}, {{binds_treatment}}) %>%
+    dplyr::group_by({{protein_id}}) %>%
+    dplyr::mutate({{is_significant}} := ifelse(sum({{is_significant}}) > 0, TRUE, FALSE)) %>%
+    dplyr::distinct()
+ 
+  cont_table <- data %>%
+    dplyr::group_by({{binds_treatment}}, {{is_significant}}) %>%
+    dplyr::summarize(n = dplyr::n_distinct(!!rlang::ensym(protein_id)), .groups = "drop") 
+  
+  fisher_test <- cont_table %>%
+    tidyr::pivot_wider(names_from = {{is_significant}}, values_from = .data$n) %>%
+    tibble::column_to_rownames(var = rlang::as_name(rlang::enquo(binds_treatment))) %>%
+    as.matrix() %>%
+    stats::fisher.test()
+  
+  cont_table <- cont_table %>%
+    dplyr::mutate(pval = fisher_test$p.value)
+  
+  if(plot == FALSE) return(cont_table)
+  
+  plot <- cont_table %>%
+    dplyr::mutate(total = sum(.data$n)) %>%
+    dplyr::mutate(
+      name = dplyr::case_when(
+        {{binds_treatment}} == FALSE &
+          {{is_significant}} == FALSE ~ "non_sig_non_interactor",
+        {{binds_treatment}} == FALSE &
+          {{is_significant}} == TRUE ~ "sig_non_interactor",
+        {{binds_treatment}} == TRUE &
+          {{is_significant}} == FALSE ~ "non_sig_interactor",
+        {{binds_treatment}} == TRUE &
+          {{is_significant}} == TRUE ~ "sig_interactor"
+      )) %>%
+     dplyr::select(-c({{binds_treatment}}, {{is_significant}})) %>%
+     tidyr::pivot_wider(names_from = .data$name, values_from = .data$n) %>%
+     dplyr::mutate(total_interactor = .data$non_sig_interactor + .data$sig_interactor) %>%
+     dplyr::mutate(total_sig = .data$sig_non_interactor + .data$sig_interactor) %>%
+     dplyr::mutate(`All detected proteins` = .data$total_interactor / .data$total * 100,
+                   `Significant proteins` = .data$sig_interactor / .data$total_sig * 100) %>%
+     tidyr::pivot_longer(
+       cols = c(.data$`All detected proteins`, .data$`Significant proteins`),
+       names_to = "name",
+       values_to = "value"
+     ) %>%
+    ggplot2::ggplot(ggplot2::aes(.data$name, .data$value)) +
+    ggplot2::geom_col(fill = "cornflowerblue", col = "black", size = 1) +
+    ggplot2::labs(title = paste0("Proteins interacting with ", treatment_name, " (p-value: ", round(cont_table$pval, digits = 3), ")"), x = "", y = paste("Interact with", treatment_name, "[%]")) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(
+        size = 20
+      ),
+      axis.text.x = ggplot2::element_text(
+        size = 15
+      ),
+      axis.text.y = ggplot2::element_text(
+        size = 15
+      ),
+      axis.title.y = ggplot2::element_text(
+        size = 15
+      )
+    )
+  if(plot == TRUE) return(plot)
+}
