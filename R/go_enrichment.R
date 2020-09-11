38d206500be25eac9f1bbb3f0e8dbc1a8e2072df
jpquast <janphilipp.quast@gmail.com>
2020-09-11 22:01:12 +0200

    Update go_enrichment based on topGO

--- R/go_enrichment.R
+++ R/go_enrichment.R
@@ -1,26 +1,36 @@
 #' Perform gene ontology enrichment analysis
 #'
-#' Analyses enrichment of gene ontology terms associated with proteins in the fraction of significant proteins compared to all detected proteins. A Fisher's 
-#' exact test is performed to test significance of enrichment. 
+#' Analyses enrichment of gene ontology terms associated with proteins in the fraction of significant proteins compared to all detected proteins. 
+#' The enrichment analysis is based on the topGO R package.
 #'
 #' @param data A data frame that contains at least the input variables.
 #' @param protein_id The name of the column containing the protein accession numbers.
 #' @param is_significant  The name of the column containing a logical indicating if the corresponding protein has a significantly changing peptide. 
 #' The input data frame may contain peptide level information with significance information. The function is able to extract protein level information from this. 
-#' @param go_annotations The name of the column containing gene ontology annotations. These can be obtained from UniProt using \code{fetch_uniprot}. 
+#' @param ontology_type A character vector specifying the type of ontology that should be used. Possible values 
+#' are molecular function (MF), biological process (BF), cellular component (CC).
+#' @param organism_id An NCBI taxonomy identifier of an organism (TaxId). Possible inputs inlude only: "9606" (Human), "559292" (Yeast) and 
+#' "83333" (E. coli). Is only necessary if gene_ontology data is not provided in \code{go_data}.
+#' @param algorithm A character vector specifying the type of algorithm that should be used to group GO terms. Default is \code{"elim"}, which
+#' simplifies GO terms. \code{"classic"} uses all available terms without simplification. Further options can be found [here](https://bioconductor.org/packages/release/bioc/vignettes/topGO/inst/doc/topGO.pdf). 
+#' @param statistic A character vector specifying the type of test that should be performed. Default is \code{"fisher"}. Alternatively,
+#' a Kolmogorov-Smirnov test can be used. Further options can be found here https://bioconductor.org/packages/release/bioc/vignettes/topGO/inst/doc/topGO.pdf.
+#' @param go_data A list. If GO annotations should not be fetched automatically, a named list can be provided. Each element should be named
+#' with an ID that corresponds to the IDs in the data. The content of each element should be a character vector with associated GO terms.
 #' @param plot A logical indicating whether the result should be plotted or returned as a table.
 #' 
-#' @return A bar plot displaying negative log10 adjusted p-values for the top 10 enriched gene ontology terms. Bars are colored according to the direction of the 
-#' enrichment. If \code{plot = FALSE}, a data frame is returned.
+#' @return A bar plot displaying negative log10 adjusted p-values for the top 10 enriched gene ontology terms. 
+#' Bars are colored according to the direction of the enrichment. If \code{plot = FALSE}, a data frame is returned.
 #'
 #' @import dplyr
 #' @import ggplot2
-#' @importFrom stringr str_split
-#' @importFrom tidyr unnest nesting complete pivot_wider separate
-#' @importFrom rlang .data := !! ensym as_name enquo
-#' @importFrom tibble column_to_rownames
+#' @import topGO
+#' @importFrom janitor clean_names
+#' @importFrom stringr str_replace
+#' @importFrom tidyr drop_na
+#' @importFrom rlang .data 
 #' @importFrom magrittr %>%
-#' @importFrom purrr map map2_df
+#' @importFrom purrr map
 #' @export
 #'
 #' @examples
@@ -28,63 +38,74 @@
 #' go_enrichment(
 #' data,
 #' protein_id = pg_protein_accessions,
-#' is_significant = significant,
-#' go_annotations = go_molecular_function
+#' is_significant = significant
+#' ontology_type = "MF",
+#' organism_id = "83333",
+#' algorithm = "elim",
+#' statistic = "fisher"
 #' )
 #' }
-go_enrichment <- function(data, protein_id, is_significant, go_annotations, plot = TRUE){
-  . = NULL
-  n_sig = NULL # to avoid node about no global variable binding. Usually this can be avoided with .data$ but not in nesting in complete function.
+go_enrichment <- function(data, protein_id, is_significant, ontology_type, organism_id, algorithm = "elim", statistic = "fisher", go_data = NULL, plot = TRUE){
+  . = NULL # to avoid note about no global variable binding. Usually this can be avoided with .data$ but not in nesting in complete function.
   
-  data <- data %>%
-    dplyr::distinct({{protein_id}}, {{is_significant}}, {{go_annotations}}) %>%
-    dplyr::group_by({{protein_id}}) %>%
-    dplyr::mutate({{is_significant}} := ifelse(sum({{is_significant}}) > 0, TRUE, FALSE)) %>% # do this to remove accidental double annotations
-    dplyr::distinct()
+  if(length(unique(dplyr::pull(data, {{protein_id}}))) != nrow(data)){
+    data <- data %>%
+      dplyr::distinct({{protein_id}}, {{is_significant}}) %>%
+      dplyr::group_by({{protein_id}}) %>%
+      dplyr::mutate({{is_significant}} := ifelse(sum({{is_significant}}) > 0, TRUE, FALSE)) %>% # do this to remove accidental double annotations
+      dplyr::distinct()
+  }
   
-  if(length(unique(pull(data, {{protein_id}}))) != nrow(data)) stop("The data frame contains more rows than unique proteins. Make sure that there
-                                                                    are no double annotations.")
-  cont_table <- data %>%
-    dplyr::group_by({{is_significant}}) %>%
-    dplyr::mutate(n_sig = dplyr::n()) %>%
-    dplyr::mutate({{go_annotations}} := stringr::str_split({{go_annotations}}, ";")) %>%
-    tidyr::unnest({{go_annotations}}) %>%
-    dplyr::mutate({{go_annotations}} := stringr::str_trim({{go_annotations}})) %>%
-    dplyr::group_by({{go_annotations}}, {{is_significant}}) %>%
-    dplyr::mutate(n_has_process = dplyr::n()) %>% # count numer of proteins with process for sig and non-sig proteins
-    dplyr::distinct({{go_annotations}}, {{is_significant}}, .data$n_sig, .data$n_has_process) %>%
-    dplyr::filter({{go_annotations}} != "") %>% # remove empty go annotations
-    dplyr::ungroup() %>%
-    tidyr::complete(!!rlang::ensym(go_annotations), tidyr::nesting(!!rlang::ensym(is_significant), n_sig), fill = list(n_has_process = 0))
+  if(is.null(go_data)){
+    go <- fetch_go(organism_id)
+    # if the provided protein_ids are not from uniprot but SGD, they are converted to uniprot 
+    if(!any(dplyr::pull(data, {{protein_id}}) %in% go$db_id)){
+      if(organism_id != "559292") stop("The provided protein IDs are not UniProt or SGD IDs or the corresponding organism is wrong. 
+                                     Please provide IDs in the correct format and check if you used the right organism ID.")
+      annotation <- fetch_uniprot_proteome(559292, columns = c("id", "database(SGD)"), reviewed = TRUE) %>% 
+        dplyr::mutate(database_sgd = stringr::str_replace(.data$database_sgd, pattern = ";", replacement = ""))
+      
+      go <- go %>% 
+        dplyr::left_join(annotation, by = c("db_id" = "database_sgd")) %>% 
+        dplyr::select(-.data$db_id) %>% 
+        dplyr::rename(db_id = .data$id)
+    }
+    
+    go_list <- go %>% 
+      dplyr::select(.data$db_id, .data$go_id) %>% 
+      dplyr::group_by(.data$db_id) %>% 
+      dplyr::summarise(go_id = list(.data$go_id), .groups = "drop") %>% 
+      tidyr::drop_na() %>% 
+      split(.$db_id) %>% 
+      purrr::map(.f = ~ unlist(.x$go_id))
+    
+  }
   
-  fisher_test <- cont_table  %>%
-    split(dplyr::pull(., !!ensym(go_annotations))) %>%
-    purrr::map(.f = ~ dplyr::select(.x, -{{go_annotations}}) %>%
-          tibble::column_to_rownames(var = rlang::as_name(enquo(is_significant))) %>%
-          as.matrix() %>%
-          fisher.test()
-          ) %>%
-    purrr::map2_df(.y = names(.),
-                   .f = ~ tibble::tibble(pval = .x$p.value,
-                                    {{go_annotations}} := .y)
-    )
+  if(!is.null(go_data)) go_list <- go_data
   
-  result_table <- cont_table %>%
-    dplyr::left_join(fisher_test, by = rlang::as_name(rlang::enquo(go_annotations))) %>%
-    dplyr::mutate(adj_pval = stats::p.adjust(.data$pval, method = "BH")) %>%
-    dplyr::group_by({{go_annotations}}) %>%
-    dplyr::mutate(n_detected_proteins = sum(.data$n_sig),
-                  n_detected_proteins_in_process = sum(.data$n_has_process),
-                  n_significant_proteins = ifelse({{is_significant}} == TRUE, .data$n_sig, NA),
-                  n_significant_proteins_in_process = ifelse({{is_significant}} == TRUE, .data$n_has_process, NA)
-                  ) %>%
-    tidyr::drop_na() %>%
-    dplyr::select(-c({{is_significant}}, .data$n_sig, .data$n_has_process)) %>%
-    dplyr::mutate(n_proteins_expected = round(.data$n_significant_proteins/.data$n_detected_proteins * .data$n_detected_proteins_in_process, digits = 2)) %>%
-    dplyr::mutate(direction = ifelse(.data$n_proteins_expected < .data$n_significant_proteins_in_process, "Up", "Down")) %>%
-    tidyr::separate({{go_annotations}}, c("term", "go_id"), "(?=\\[GO:)") %>%
-    dplyr::arrange(.data$pval)
-
+  protein_list <- factor(as.integer(dplyr::pull(data, {{is_significant}})))
+  names(protein_list) <- dplyr::pull(data, {{protein_id}})
+  
+  go_data <- methods::new("topGOdata", 
+                 ontology = ontology_type, 
+                 allGenes = protein_list,
+                 annot = annFUN.gene2GO,
+                 gene2GO = go_list)
+  
+  result <- topGO::runTest(go_data, algorithm = algorithm, statistic = statistic)
+  
+  result_table <- topGO::GenTable(go_data, 
+                                  pval = result, 
+                                  orderBy = "pval", 
+                                  topNodes = length(result@score), 
+                                  numChar = 100) %>% 
+    dplyr::mutate(elim = algorithm == "elim") %>% 
+    dplyr::mutate(pval = as.numeric(.data$pval)) %>% 
+    dplyr::mutate(adj_pval = ifelse(elim, .data$pval, stats::p.adjust(.data$pval, method = "BH"))) %>%
+    dplyr::select(-elim) %>% 
+    janitor::clean_names() %>% 
+    dplyr::mutate(direction = ifelse(.data$expected < .data$significant, "Up", "Down"))
+  
   if(plot == FALSE) return(result_table)
   
   enrichment_plot <- result_table %>%
@@ -92,8 +113,8 @@ go_enrichment <- function(data, protein_id, is_significant, go_annotations, plot
     dplyr::slice(1:10) %>%
     ggplot2::ggplot(ggplot2::aes(stats::reorder(.data$term, .data$neg_log_qval), .data$neg_log_qval, fill = .data$direction)) +
     ggplot2::geom_col(col = "black", size = 1.5) +
-    ggplot2::scale_fill_manual(values = c("#56B4E9", "#E76145")) +
-    ggplot2::scale_y_continuous(breaks = seq(0, 100, 2)) +
+    ggplot2::scale_fill_manual(values = c(Down = "#56B4E9", Up = "#E76145")) +
+    ggplot2::scale_y_continuous(breaks = seq(0, 100, 1)) +
     ggplot2::coord_flip() +
     ggplot2::labs(title = "Gene ontology enrichment of significant proteins", y = "-log10 adjusted p-value") +
     ggplot2::theme_bw() +
