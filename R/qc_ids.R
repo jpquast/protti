99feb9fefb5a4445f82cdb30e3ac7a74a473431a
Dina Schuster <dschuster@ethz.ch>
2020-09-21 10:39:14 +0200

    qc_ids color update

--- R/qc_ids.R
+++ R/qc_ids.R
@@ -47,7 +47,23 @@ qc_ids <-
            x = "sample",
            y = "count") +
       theme_bw() +
-      theme(axis.text.x = element_text(angle = 75, hjust = 1))
+      theme(axis.text.x = element_text(angle = 75, hjust = 1)) +
+      scale_fill_manual(values = c("#5680C1",
+                                   "#B96DAD",
+                                   "#64CACA",
+                                   "#81ABE9",
+                                   "#F6B8D1",
+                                   "#99F1E4",
+                                   "#9AD1FF",
+                                   "#548BDF",
+                                   "#A55098",
+                                   "#3EB6B6",
+                                   "#87AEE8",
+                                   "#CA91C1",
+                                   "#A4E0E0",
+                                   "#1D4F9A",
+                                   "#D7ACD2",
+                                   "#49C1C1"))
     if (interactive == TRUE)
     {
       return(plotly::ggplotly(plot))
