75897dd4913c13d24fd084156c4787f7e40298d5
Dina Schuster <dschuster@ethz.ch>
2020-10-05 18:33:15 +0200

    Median normalisation corrections

--- R/median_normalisation.R
+++ R/median_normalisation.R
@@ -5,14 +5,14 @@
 #' @param data A data frame containing at least sample names, grouping variables and intensity values.
 #' @param sample The name of the column containing the sample names.
 #' @param grouping The name of the column containing the grouping variable - this can be peptides, precursors or proteins.
-#' @param log2intensity The name of the column containing the log2 transformed intensity values to be normalised.
-#' @param na.rm Logical indicating whether missing values should be removed. Default is TRUE.
+#' @param intensity_log2 The name of the column containing the log2 transformed intensity values to be normalised.
 #'
-#' @return A new column in the original dataframe called \code{normalised_intensity_log2} containing the normalised intensity values.
+#' @return A dataframe with a column called \code{normalised_intensity_log2} containing the normalised intensity values.
 #' @import dplyr
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 #' @importFrom stats median
+#' @importFrom tidyr drop_na
 #' @export
 #'
 #' @examples
@@ -20,17 +20,17 @@
 #' median_normalisation(data,
 #' sample = r_file_name,
 #' grouping = eg_precursor_id,
-#' log2intensity = intensity_log2)
+#' intensity_log2 = intensity_log2)
 #' }
 median_normalisation <-
-  function(data, sample, grouping, log2intensity, na.rm = TRUE)
+  function(data, sample, grouping, intensity_log2)
   {
     data %>%
-      dplyr::filter(!is.na({{log2intensity}})) %>%
-      dplyr::mutate(global_median = stats::median({{log2intensity}}), na.rm = na.rm) %>%
+      tidyr::drop_na({{intensity_log2}}) %>%
+      dplyr::mutate(global_median = stats::median({{intensity_log2}})) %>%
       dplyr::group_by({{sample}}) %>%
-      dplyr::mutate(run_median = stats::median({{log2intensity}}), na.rm = na.rm) %>%
+      dplyr::mutate(run_median = stats::median({{intensity_log2}})) %>%
       dplyr::ungroup() %>%
-      dplyr::mutate(normalised_intensity_log2 = {{log2intensity}} - .data$run_median + .data$global_median) %>%
+      dplyr::mutate(normalised_intensity_log2 = {{intensity_log2}} - .data$run_median + .data$global_median) %>%
       dplyr::select(-.data$run_median, -.data$global_median)
   }
