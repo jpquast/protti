724aa194c6b03f478c05e0a37f2d3f3bbf506edd
Jan-Philipp Quast <janphilipp.quast@gmail.com>
2020-11-02 13:33:14 +0100

    Add documentation and further options to create_queue

--- R/create_queue.R
+++ R/create_queue.R
@@ -12,20 +12,26 @@
 #' @param treatment_type_1 Optional, name of the treatment.
 #' @param treatment_type_2 Optinal, name of a second treatment that was combined with the first treatment.
 #' @param treatment_dose_1 Optional, doses used for treatment 1. These can be concentrations or times etc.
+#' @param treatment_dose_2 Optional, doses used for treatment 2. These can be concentrations or times etc.
 #' @param treatment_unit_1 Optional, unit of the doses for treatment 1 (e.g min, mM, etc.).
+#' @param treatment_unit_2 Optional, unit of the doses for treatment 2 (e.g min, mM, etc.).
 #' @param n_replicates Optional, number of replicates used per sample.
+#' @param number_runs Specifies if file names should be numbered from 1:n instead of adding experiment information. Default is FALSE.
 #' @param organism Optional, name of the organism used.
+#' @param exclude_combinations Optional, list of lists that contains vectors treatment types and treatment doses whos combinations should be 
+#' excluded from the final queue. 
 #' @param inj_vol The volume used for injection in microliter. Will be \code{NA} if not specified, but needs to be specified later on then.
 #' @param data_path The file path where the MS raw data should be saved. Backslashes should be escaped by another backslash. Will be \code{NA} if not specified, but needs to be specified later on then.
-#' @param method_path The file path of the MS method for the acquisition method. Backslashes should be escaped by another backslash. Will be \code{NA} if not specified, but needs to be specified later on then.
+#' @param method_path The file path of the MS acquisition method. Backslashes should be escaped by another backslash. Will be \code{NA} if not specified, but needs to be specified later on then.
 #' @param position_row The row positions that can be used for the samples (e.g c("A", "B")). If the number of specified rows and columns does not equal the total number 
 #' of samples, positions will be repeated.
 #' @param position_column The column positions that can be used for the samples (e.g 8). If the number of specified rows and columns does not equal the total number 
 #' of samples, positions will be repeated.
-#' @param blank_every_n 
-#' @param blank_position
-#' @param blank_method_path
-#' @param blank_inj_vol
+#' @param blank_every_n Optional, specifies in which intervals a blank sample should be inserted.
+#' @param blank_position The plate position of the blank. Will be \code{NA} if not specified, but needs to be specified later on then.
+#' @param blank_method_path The file path of the MS acquisition method of the blank. Backslashes should be escaped by another backslash. 
+#' Will be \code{NA} if not specified, but needs to be specified later on then.
+#' @param blank_inj_vol Injection volume of the blank sample. Will be \code{NA} if not specified, but needs to be specified later on then.
 #' @param export_to_queue Logical, specifying if the resulting queue should be appended to an already existing queue. If false result will be saved as \code{queue.csv}.
 #' @param queue_path Optional, file path to a queue file to which the generated queue should be appended if \code{export_to_queue = TRUE}. 
 #' If not specified queue file can be chosen interactively.
@@ -35,11 +41,37 @@
 #' @importFrom tidyr crossing unite
 #' @importFrom dplyr mutate select bind_cols
 #' @importFrom data.table fread fwrite
+#' @importFrom purrr map_dfr map2_dfr
+#' @importFrom tibble rowid_to_column
+#' @importFrom rlang .data
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' create_queue(rows = 195:235, export = TRUE)
+#' create_queue(date = c("200722"),
+#' instrument = c("EX1"),
+#' user = c("jquast"),
+#' measurement_type = c("DIA"),
+#' experiment_name = c("JPQ031"),
+#' digestion = c("LiP", "tryptic control"),
+#' treatment_type_1 = c("EDTA", "H2O"),
+#' treatment_type_2 = c("Zeba", "unfiltered"),
+#' treatment_dose_1 = c(10, 30, 60),
+#' treatment_unit_1 = c("min"),
+#' n_replicates = 4,
+#' number_runs = FALSE,
+#' organism = c("E. coli"),
+#' exclude_combinations = list(list(treatment_type_1 = c("H2O"),
+#'                                  treatment_type_2 = c("Zeba", "unfiltered"),
+#'                                  treatment_dose_1 = c(10, 30))),
+#' inj_vol = c(2),
+#' data_path = "D:\\2007_Data",
+#' method_path = "C:\\Xcalibur\\methods\\jquast\\DIA_120min_41var_AGC200",
+#' position_row = c("A", "B", "C", "D", "E", "F"),
+#' position_column = 8,
+#' blank_every_n = 4,
+#' blank_position = "1-V1",
+#' blank_method_path = "C:\\Xcalibur\\methods\\blank")
 #' }
 create_queue <-
   function(date = NULL,
@@ -51,9 +83,13 @@ create_queue <-
            treatment_type_1 = NULL,
            treatment_type_2 = NULL,
            treatment_dose_1 = NULL,
+           treatment_dose_2 = NULL,
            treatment_unit_1 = NULL,
+           treatment_unit_2 = NULL,
            n_replicates = NULL,
+           number_runs = FALSE,
            organism = NULL,
+           exclude_combinations = NULL,
            inj_vol = NA,
            data_path = NA,
            method_path = NA,
@@ -63,7 +99,7 @@ create_queue <-
            blank_position = NA,
            blank_method_path = NA,
            blank_inj_vol = 1,
-           export_to_queue = TRUE,
+           export_to_queue = FALSE,
            queue_path = NULL) {
     data <-
       tidyr::crossing(
@@ -76,7 +112,9 @@ create_queue <-
         treatment_type_2,
         treatment_type_1,
         treatment_dose_1,
+        treatment_dose_2,
         treatment_unit_1,
+        treatment_unit_2,
         1:n_replicates
       )
     
@@ -87,32 +125,124 @@ create_queue <-
         treatment_type_2,
         treatment_type_1,
         treatment_dose_1,
+        treatment_dose_2,
         treatment_unit_1,
+        treatment_unit_2,
         1:n_replicates
-      ) %>%
-      tidyr::unite(`Sample Name`, sep = " ")
+      )
+    
+    if (!is.null(exclude_combinations)) {
+      exclude <- purrr::map_dfr(
+        .x = exclude_combinations,
+        .f = ~ tidyr::crossing(
+          .x$treatment_type_2,
+          .x$treatment_type_1,
+          .x$treatment_dose_1,
+          .x$treatment_dose_2
+        )
+      )
+      
+      data <- data %>%
+        dplyr::mutate(
+          t1_null = is.null(treatment_type_1),
+          t2_null = is.null(treatment_type_2),
+          d1_null = is.null(treatment_dose_1),
+          d2_null = is.null(treatment_dose_2)
+        ) %>%
+        dplyr::filter(!(
+          ifelse(
+            !.data$t2_null,
+            treatment_type_2 %in% exclude$`.x$treatment_type_2`,
+            TRUE
+          ) &
+            ifelse(
+              !.data$t1_null,
+              treatment_type_1 %in% exclude$`.x$treatment_type_1`,
+              TRUE
+            ) &
+            ifelse(
+              !.data$d2_null,
+              treatment_dose_2 %in% exclude$`.x$treatment_dose_2`,
+              TRUE
+            ) &
+            ifelse(
+              !.data$d1_null,
+              treatment_dose_1 %in% exclude$`.x$treatment_dose_1`,
+              TRUE
+            )
+        )) %>%
+        dplyr::select(-c(.data$t1_null, .data$t2_null, .data$d1_null, .data$d2_null))
+      
+      sample_name <- sample_name %>%
+        dplyr::mutate(
+          t1_null = is.null(treatment_type_1),
+          t2_null = is.null(treatment_type_2),
+          d1_null = is.null(treatment_dose_1),
+          d2_null = is.null(treatment_dose_2)
+        ) %>%
+        dplyr::filter(!(
+          ifelse(
+            !.data$t2_null,
+            treatment_type_2 %in% exclude$`.x$treatment_type_2`,
+            TRUE
+          ) &
+            ifelse(
+              !.data$t1_null,
+              treatment_type_1 %in% exclude$`.x$treatment_type_1`,
+              TRUE
+            ) &
+            ifelse(
+              !.data$d2_null,
+              treatment_dose_2 %in% exclude$`.x$treatment_dose_2`,
+              TRUE
+            ) &
+            ifelse(
+              !.data$d1_null,
+              treatment_dose_1 %in% exclude$`.x$treatment_dose_1`,
+              TRUE
+            )
+        )) %>%
+        dplyr::select(-c(.data$t1_null, .data$t2_null, .data$d1_null, .data$d2_null))
+    }
+    
+    if (number_runs == TRUE) {
+      data <- data %>%
+        tibble::rowid_to_column("number") %>%
+        dplyr::select(date,
+                      instrument,
+                      user,
+                      measurement_type,
+                      experiment_name,
+                      .data$number)
+    }
+    
+    data <- data %>%
+      tidyr::unite("File Name", sep = "_")
+    
+    sample_name <- sample_name %>%
+      tidyr::unite("Sample Name", sep = " ")
     
     nrow_data <- nrow(data)
     
     position <- tidyr::crossing(position_row, 1:position_column) %>%
-      tidyr::unite(`Position`, sep = "")
+      tidyr::unite("Position", sep = "")
     
     nrow_position <- nrow(position)
     
-    ratio <- ceiling(nrow_data/nrow_position)
+    ratio <- ceiling(nrow_data / nrow_position)
     position_final <- NULL
     
-    for(i in 1:ratio){
-      position_final <- position_final %>% 
+    for (i in 1:ratio) {
+      position_final <- position_final %>%
         dplyr::bind_rows(position)
     }
     
     position_final <- position_final %>%
       dplyr::slice(1:nrow_data)
     
-    result <- tidyr::unite(data, `File Name`, sep = "_") %>%
+    result <- data %>%
       dplyr::mutate(`Sample Type` = "Unknown") %>%
-      dplyr::select(`Sample Type`, `File Name`) %>%
+      dplyr::select(.data$`Sample Type`, .data$`File Name`) %>%
       dplyr::mutate(`Sample ID` = 1) %>%
       dplyr::mutate(Path = data_path) %>%
       dplyr::mutate(`Instrument Method` = method_path) %>%
@@ -132,15 +262,16 @@ create_queue <-
       dplyr::mutate(`L5 Phone` = NA) %>%
       dplyr::mutate(`Comment` = NA) %>%
       dplyr::bind_cols(sample_name)
-
-    if (!is.null(blank_every_n)){
-      n_blanks <- nrow(result)/blank_every_n
+    
+    if (!is.null(blank_every_n)) {
+      n_blanks <- nrow(result) / blank_every_n
       
-      blank_names <- tidyr::crossing(date, instrument, user, "blank", 1:n_blanks) 
+      blank_names <-
+        tidyr::crossing(date, instrument, user, "blank", 1:n_blanks)
       
-      blank <- tidyr::unite(blank_names, `File Name`, sep = "_") %>%
+      blank <- tidyr::unite(blank_names, "File Name", sep = "_") %>%
         dplyr::mutate(`Sample Type` = "QC") %>%
-        dplyr::select(`Sample Type`, `File Name`) %>%
+        dplyr::select(.data$`Sample Type`, .data$`File Name`) %>%
         dplyr::mutate(`Sample ID` = 1) %>%
         dplyr::mutate(Path = data_path) %>%
         dplyr::mutate(`Instrument Method` = blank_method_path) %>%
@@ -159,20 +290,22 @@ create_queue <-
         dplyr::mutate(`L4 Company` = NA) %>%
         dplyr::mutate(`L5 Phone` = NA) %>%
         dplyr::mutate(`Comment` = NA) %>%
-        dplyr::mutate(`Sample Name` = "blank") %>% 
+        dplyr::mutate(`Sample Name` = "blank") %>%
         split(1:n_blanks)
       
-      result <- result %>% 
-        split((as.numeric(rownames(result))-1) %/% 4) %>% 
+      result <- result %>%
+        split((as.numeric(rownames(result)) - 1) %/% 4) %>%
         purrr::map2_dfr(.y = blank,
-                 .f = ~ .y %>% 
-                   dplyr::bind_rows(.x))
+                        .f = ~ .y %>%
+                          dplyr::bind_rows(.x))
     }
-    
     if (export_to_queue == FALSE) {
       # a file named queue.csv is exported
       write("Bracket Type=4", file = "queue.csv")
-      data.table::fwrite(result, "queue.csv")
+      data.table::fwrite(result,
+                         "queue.csv",
+                         append = TRUE,
+                         col.names = TRUE)
     }
     if (export_to_queue == TRUE) {
       # load queue interactively if no path is provided in the queue_path argument
@@ -180,33 +313,14 @@ create_queue <-
         queue_path <- file.choose(".")
       }
       queue <- data.table::fread(queue_path, skip = 1)
-
+      
       queue <- queue %>%
         dplyr::bind_rows(result)
-
+      
       write("Bracket Type=4", file = queue_path)
-      data.table::fwrite(queue, file = queue_path, append = TRUE, col.names = TRUE)
+      data.table::fwrite(queue,
+                         file = queue_path,
+                         append = TRUE,
+                         col.names = TRUE)
     }
   }
-
-test <- create_queue(date = c("200722"),
-             instrument = c("EX1"),
-             user = c("jquast"),
-             measurement_type = c("DIA"),
-             experiment_name = c("JPQ031"),
-             digestion = c("LiP", "tryptic control"),
-             treatment_type_1 = c("EDTA", "H2O"),
-             treatment_type_2 = c("Zeba", "unfiltered"),
-             treatment_dose_1 = c(0, 30, 60),
-             treatment_unit_1 = c("min"),
-             n_replicates = 4,
-             organism = c("E. coli"),
-             inj_vol = c(2),
-             data_path = "D:\\2007_Data",
-             method_path = "C:\\Xcalibur\\methods\\jquast\\DIA_120min_41var_AGC200",
-             position_row = c("A", "B", "C", "D", "E", "F"),
-             position_column = 8,
-             blank_every_n = 4,
-             blank_position = "1-V1",
-             blank_method_path = "C:\\Xcalibur\\methods\\blank")
-
