fe344688c7f5bd4043fc74cebf1c9b72d743aa37
Dina Schuster <dschuster@ethz.ch>
2020-11-16 17:21:53 +0100

    qc_cvs update

--- R/qc_cvs.R
+++ R/qc_cvs.R
@@ -5,7 +5,7 @@
 #' @param data A dataframe containing at least peptide, precursor or protein identifiers, information on conditions and intensity values for each peptide, precursor or protein.
 #' @param grouping The column in the input dataframe containing the grouping variables (e.g. peptides, precursors or proteins).
 #' @param condition The column in the data dataframe containing condition information (e.g. "treated" and "control").
-#' @param intensity Column containing the corresponding intensity values for each peptide or precursor.
+#' @param intensity Column containing the corresponding raw intensity values (not log2 transformed) for each peptide or precursor.
 #' @param plot A logical indicating whether the result should be plotted. Default is TRUE.
 #' @param plot_style Variable indicating the plotting style. \code{plot_style = "boxplot"} plots a boxplot, whereas \code{plot_style = "density"} plots the CV density distribution. Default is \code{plot_style = "density"}.
 #'
@@ -34,31 +34,68 @@ qc_cvs <-
   {
     if(plot == FALSE)
     {
+      if(max(dplyr::pull(data, {{intensity}})) < 1000)
+      {
+      stop("Please backtransform your data or use raw values. The function does not handle log2 transformed data.")
+      }
+
       result <- data %>%
+        dplyr::select({{grouping}}, {{condition}}, {{intensity}}) %>%
         tidyr::drop_na({{intensity}}) %>%
+        dplyr::group_by({{grouping}}) %>%
+        dplyr::mutate(cv_combined = (stats::sd({{intensity}}) / mean({{intensity}})) * 100) %>%
         dplyr::group_by({{condition}}, {{grouping}}) %>%
         dplyr::mutate(cv = (stats::sd({{intensity}}) / mean({{intensity}})) * 100) %>%
+        dplyr::select({{condition}}, {{grouping}}, .data$cv_combined, .data$cv) %>%
+        dplyr::distinct() %>%
+        tidyr::drop_na() %>%
         dplyr::group_by({{condition}}) %>%
-        dplyr::summarise(median_cv = stats::median(.data$cv))
+        dplyr::mutate(median_cv = stats::median(.data$cv)) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(median_cv_combined = stats::median(.data$cv_combined)) %>%
+        dplyr::select(- {{grouping}}, - .data$cv_combined, - .data$cv) %>%
+        dplyr::distinct()
 
       return(result)
     }
 
     if(plot == TRUE)
     {
+      if(max(dplyr::pull(data, {{intensity}})) < 1000)
+      {
+        stop("Please backtransform your data or use raw values. The function does not handle log2 transformed data.")
+      }
       result <- data %>%
-      dplyr::distinct({{condition}}, {{grouping}}, {{intensity}}) %>%
-      dplyr::group_by({{condition}}, {{grouping}}) %>%
-      dplyr::mutate(cv = (stats::sd({{intensity}}) / mean({{intensity}})) * 100)
+        tidyr::drop_na({{intensity}}) %>%
+        dplyr::group_by({{grouping}}) %>%
+        dplyr::mutate(cv_combined = (stats::sd({{intensity}}) / mean({{intensity}})) * 100) %>%
+        dplyr::group_by({{condition}}, {{grouping}}) %>%
+        dplyr::mutate(cv = (stats::sd({{intensity}}) / mean({{intensity}})) * 100) %>%
+        dplyr::ungroup() %>%
+        dplyr::select({{condition}}, {{grouping}}, .data$cv_combined, .data$cv) %>%
+        dplyr::distinct() %>%
+        tidyr::drop_na() %>%
+        tidyr::pivot_longer(cols = starts_with("cv"), names_to = "type", values_to = "values") %>%
+        dplyr::mutate(type = ifelse(.data$type == "cv", {{condition}}, "combined")) %>%
+        dplyr::select(-{{condition}}) %>%
+        dplyr::distinct()
+
+      if (max(result$values) > 200) {
+        cv_too_high <- result %>% dplyr::filter(.data$values >= 200) %>% nrow()
+        warning(paste(cv_too_high), " values were exluded from the plot (CV > 200 %)")
+      }
 
     if(plot_style == "boxplot")
     {
-      plot <- ggplot2::ggplot(result, aes(x = {{condition}}, y = .data$cv, fill = {{condition}})) +
-        ggplot2::geom_boxplot() +
+      plot <- ggplot2::ggplot(result) +
+        ggplot2::geom_boxplot(aes(x = .data$type, y = .data$values, fill = .data$type)) +
         ggplot2::theme_bw() +
         ggplot2::ylab("Coefficient of variation [%]") +
         ggplot2::labs(title = "Coefficients of variation") +
-        ggplot2::scale_fill_manual(values = c("#5680C1",
+        ggplot2::theme(axis.text.x = element_text(angle = 75, hjust = 1)) +
+        ggplot2::scale_y_continuous(limits = c(0, 200)) +
+        ggplot2::scale_fill_manual(values = c("#8D8D8D",
+                                              "#5680C1",
                                               "#B96DAD",
                                               "#64CACA",
                                               "#81ABE9",
@@ -79,13 +116,15 @@ qc_cvs <-
     }
     if(plot_style == "density")
     {
-      plot <- ggplot2::ggplot(result, aes(x = .data$cv, col = {{condition}})) +
-        ggplot2::geom_density(size = 1) +
+      plot <- ggplot2::ggplot(result) +
+        ggplot2::geom_density(aes(x = .data$values, col = .data$type), size = 1) +
         ggplot2::theme_bw() +
         ggplot2::ylab("Density") +
         ggplot2::labs(title = "Coefficients of variation") +
         ggplot2::xlab("Coefficient of variation [%]") +
-        ggplot2::scale_color_manual(values = c("#5680C1",
+        ggplot2::scale_x_continuous(limits = c(0, 200)) +
+        ggplot2::scale_color_manual(values = c("#8D8D8D",
+                                              "#5680C1",
                                               "#B96DAD",
                                               "#64CACA",
                                               "#81ABE9",
