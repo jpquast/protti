e723f8abaf1423e60f6bec775af15c9b60be138c
jpquast <janphilipp.quast@gmail.com>
2021-01-18 14:28:21 +0100

    fix startup message note

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(cat("\U1F469\U1F3FD\U200D\U1F52C Welcome to protti! \U1F468\U1F3FC\U200D\U1F4BB 
-                            \n\U1F52C Have fun analysing your data! \U1F4BB"))
+  packageStartupMessage("\U1F469\U1F3FD\U200D\U1F52C Welcome to protti version ", utils::packageVersion("protti"), "! \U1F468\U1F3FC\U200D\U1F4BB 
+                            \n\U1F52C Have fun analysing your data! \U1F4BB")
 }
