cbec16c2c06c0a092390716f9c26047e3b318600
jpquast <janphilipp.quast@gmail.com>
2021-03-02 11:16:29 +0100

    Add example data

--- R/data.R
+++ R/data.R
@@ -5,3 +5,23 @@
 #' @format A vector containing 100 colours
 #' @source Dina's imagination.
 "protti_colours"
+
+#' Rapamycin 10 uM example data 
+#'
+#' Rapamycin example data used for the vignette about binary control/treated data. The data is obtained from \href{https://www.nature.com/articles/s41467-020-18071-x}{Piazza 2020}
+#' and corresponds to experiment 18. FKBP1A the rapamycin binding protein and 49 other randomly sampled proteins were used 
+#' for this example dataset. Furthermore, only the DMSO control and the 10 uM condition were used. 
+#'
+#' @format A data frame containing peptide level data from a Spectronaut report.
+#' @source Piazza, I., Beaton, N., Bruderer, R. et al. A machine learning-based chemoproteomic approach to identify drug targets and binding sites in complex proteomes. Nat Commun 11, 4200 (2020). https://doi.org/10.1038/s41467-020-18071-x
+"rapamycin_10uM"
+
+#' Rapamycin dose response example data
+#'
+#' Rapamycin example data used for the vignette about dose response data. The data is obtained from \href{https://www.nature.com/articles/s41467-020-18071-x}{Piazza 2020}
+#' and corresponds to experiment 18. FKBP1A the rapamycin binding protein and 86 (to not exceed file size limit) other randomly sampled proteins were used 
+#' for this example dataset. The concentration range includes the following points: 0 (DMSO control), 10 pM, 100 pM, 1 nM, 10 nM, 100 nM, 1 uM, 10 uM and 100 uM.
+#'
+#' @format A data frame containing peptide level data from a Spectronaut report.
+#' @source Piazza, I., Beaton, N., Bruderer, R. et al. A machine learning-based chemoproteomic approach to identify drug targets and binding sites in complex proteomes. Nat Commun 11, 4200 (2020). https://doi.org/10.1038/s41467-020-18071-x
+"rapamycin_dose_response"
