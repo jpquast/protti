b55f19fde2295e96a2e09a84b931a4d088bec8df
Dina Schuster <dschuster@ethz.ch>
2021-06-15 10:54:11 +0200

    Woods plot improvements
    Added an option to highlight significant peptides with an asterisk, changed the peptide rectangle height, made peptide rectangles transparent

--- R/woods_plot.R
+++ R/woods_plot.R
@@ -15,7 +15,10 @@
 #' protein identifiers.
 #' @param colouring Optional argument, column in the data frame containing information by which peptide or precursors should
 #' be coloured.
-#' @param fold_change_cutoff Optional argument specifying the log2 fold change cutoff used for assessing whether changes are significant. The default value is 2.
+#' @param fold_change_cutoff Optional argument specifying the log2 fold change cutoff used in the plot. The default value is 2.
+#' @param highlight_significant Logical, specifying whether significantly changing peptides should be highlighted with an asterisk. Default is \code{highlight_significant = FALSE}.
+#' @param significance Required if \code{highlight_significant = TRUE}. Argument specifies the column in the input data frame containing significance values (p-values, q-values).
+#' @param significance_cutoff Required if \code{highlight_significant = TRUE}. Argument specifies the significance cutoff below which peptides are considered significant.
 #'
 #' @return A Wood's plot is returned. Plotting peptide or precursor fold changes accross protein sequence.
 #' @import ggplot2
@@ -33,10 +36,13 @@
 #'   end_position = end,
 #'   protein_length = length,
 #'   colouring = pep_type,
-#'   facet = pg_protein_accessions
+#'   facet = pg_protein_accessions, 
+#'   highlight_significant = TRUE, 
+#'   significance = adj_pval, 
+#'   significance_cutoff = 0.01
 #' )
 #' }
-woods_plot <- function(data, fold_change, start_position, end_position, protein_length, coverage = NULL, protein_id = NULL, facet = NULL, colouring = NULL, fold_change_cutoff = 1) {
+woods_plot <- function(data, fold_change, start_position, end_position, protein_length, coverage = NULL, protein_id = NULL, facet = NULL, colouring = NULL, fold_change_cutoff = 1, highlight_significant = FALSE, significance = NULL, significance_cutoff = NULL) {
   protti_colours <- "placeholder" # assign a placeholder to prevent a missing global variable warning
   utils::data("protti_colours", envir = environment()) # then overwrite it with real data
   # Check if there are more than one protein even though protein_id was specified.
@@ -69,86 +75,184 @@ woods_plot <- function(data, fold_change, start_position, end_position, protein_
       dplyr::mutate({{ protein_id }} := paste0({{ protein_id }}, " (", round({{ coverage }}, digits = 1), "%)"))
   }
   # Create plot
-  data %>%
-    ggplot2::ggplot() +
-    ggplot2::geom_rect(ggplot2::aes(
-      xmin = 0,
-      xmax = {{ protein_length }},
-      ymin = -0.01,
-      ymax = 0.01
-    ),
-    fill = "black"
-    ) +
-    ggplot2::geom_rect(ggplot2::aes(
-      xmin = {{ start_position }},
-      xmax = {{ end_position }},
-      ymin = {{ fold_change }} - 0.5,
-      ymax = {{ fold_change }} + 0.5,
-      fill = {{ colouring }}
-    ),
-    col = "black",
-    size = 0.7
-    ) +
-    ggplot2::geom_hline(
-      yintercept = -{{ fold_change_cutoff }},
-      col = "blue",
-      alpha = .8,
-      size = 0.7
-    ) +
-    ggplot2::geom_hline(
-      yintercept = {{ fold_change_cutoff }},
-      col = "blue",
-      alpha = .8,
-      size = 0.7
-    ) +
-    ggplot2::ylim(min(-2.5, dplyr::pull(data, {{ fold_change }})) - 0.5, max(2.5, dplyr::pull(data, {{ fold_change }})) + 0.5) +
-    ggplot2::scale_x_continuous(limits = NULL, expand = c(0, 0)) +
-    ggplot2::labs(
-      x = "Protein Sequence",
-      y = "log2(fold change)",
-      title = {
-        if (!missing(protein_id)) unique(dplyr::pull(data, {{ protein_id }}))
-      }
-    ) +
-    {
-      if (!missing(facet)) ggplot2::facet_wrap(rlang::new_formula(NULL, rlang::enquo(facet)), scales = "free", ncol = 4)
-    } +
-    ggplot2::guides(size = FALSE) +
-    {
-      if (!missing(colouring) && !is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_manual(values = protti_colours)
-    } +
-    {
-      if (!missing(colouring) && is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_gradient(low = protti_colours[1], high = protti_colours[2])
-    } +
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      plot.title = ggplot2::element_text(
-        size = 20
+  if((highlight_significant == TRUE & missing(significance)) | (highlight_significant == TRUE & missing(significance_cutoff))){
+    warning(paste("Please provide the required columns (significance and significance_cutoff) to highlight significant peptides."))
+  }
+  if(highlight_significant == TRUE & !missing(significance) & !missing(significance_cutoff)) {
+    data %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_rect(ggplot2::aes(
+        xmin = 0,
+        xmax = {{ protein_length }},
+        ymin = -0.01,
+        ymax = 0.01
       ),
-      axis.text.x = ggplot2::element_text(
-        size = 15
+      fill = "black"
+      ) +
+      ggplot2::geom_rect(ggplot2::aes(
+        xmin = {{ start_position }},
+        xmax = {{ end_position }},
+        ymin = {{ fold_change }} - 0.2,
+        ymax = {{ fold_change }} + 0.2,
+        fill = {{ colouring }}
       ),
-      axis.text.y = ggplot2::element_text(
-        size = 15
+      col = "black",
+      size = 0.7,
+      alpha = 0.8
+      ) +
+      ggplot2::geom_point(data = dplyr::filter(data, {{ significance }} < significance_cutoff),
+                          ggplot2::aes(
+                            x = (( {{ start_position }} + {{ end_position }} ) /2),
+                            y = ( {{ fold_change }} - 0.3)), 
+                          shape = 8, 
+                          col = "black", 
+                          size = 3) +
+      ggplot2::geom_hline(
+        yintercept = -{{ fold_change_cutoff }},
+        col = "blue",
+        alpha = .8,
+        size = 0.7
+      ) +
+      ggplot2::geom_hline(
+        yintercept = {{ fold_change_cutoff }},
+        col = "blue",
+        alpha = .8,
+        size = 0.7
+      ) +
+      ggplot2::ylim(min(-2.5, dplyr::pull(data, {{ fold_change }})) - 0.5, max(2.5, dplyr::pull(data, {{ fold_change }})) + 0.5) +
+      ggplot2::scale_x_continuous(limits = NULL, expand = c(0, 0)) +
+      ggplot2::labs(
+        x = "Protein Sequence",
+        y = "log2(fold change)",
+        title = {
+          if (!missing(protein_id)) unique(dplyr::pull(data, {{ protein_id }}))
+        }
+      ) +
+      {
+        if (!missing(facet)) ggplot2::facet_wrap(rlang::new_formula(NULL, rlang::enquo(facet)), scales = "free", ncol = 4)
+      } +
+      ggplot2::guides(size = FALSE) +
+      {
+        if (!missing(colouring) && !is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_manual(values = protti_colours)
+      } +
+      {
+        if (!missing(colouring) && is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_gradient(low = protti_colours[1], high = protti_colours[2])
+      } +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(
+          size = 20
+        ),
+        axis.text.x = ggplot2::element_text(
+          size = 15
+        ),
+        axis.text.y = ggplot2::element_text(
+          size = 15
+        ),
+        axis.title.y = ggplot2::element_text(
+          size = 15
+        ),
+        axis.title.x = ggplot2::element_text(
+          size = 15
+        ),
+        legend.title = ggplot2::element_text(
+          size = 15
+        ),
+        legend.text = ggplot2::element_text(
+          size = 15
+        ),
+        strip.text.x = ggplot2::element_text(
+          size = 15
+        ),
+        strip.text = ggplot2::element_text(
+          size = 15
+        ),
+        strip.background = element_blank()
+      )
+  }else{
+    
+    data %>%
+      ggplot2::ggplot() +
+      ggplot2::geom_rect(ggplot2::aes(
+        xmin = 0,
+        xmax = {{ protein_length }},
+        ymin = -0.01,
+        ymax = 0.01
       ),
-      axis.title.y = ggplot2::element_text(
-        size = 15
+      fill = "black"
+      ) +
+      ggplot2::geom_rect(ggplot2::aes(
+        xmin = {{ start_position }},
+        xmax = {{ end_position }},
+        ymin = {{ fold_change }} - 0.5,
+        ymax = {{ fold_change }} + 0.5,
+        fill = {{ colouring }}
       ),
-      axis.title.x = ggplot2::element_text(
-        size = 15
-      ),
-      legend.title = ggplot2::element_text(
-        size = 15
-      ),
-      legend.text = ggplot2::element_text(
-        size = 15
-      ),
-      strip.text.x = ggplot2::element_text(
-        size = 15
-      ),
-      strip.text = ggplot2::element_text(
-        size = 15
-      ),
-      strip.background = element_blank()
-    )
+      col = "black",
+      size = 0.7,
+      alpha = 0.8
+      ) +
+      ggplot2::geom_hline(
+        yintercept = -{{ fold_change_cutoff }},
+        col = "blue",
+        alpha = .8,
+        size = 0.7
+      ) +
+      ggplot2::geom_hline(
+        yintercept = {{ fold_change_cutoff }},
+        col = "blue",
+        alpha = .8,
+        size = 0.7
+      ) +
+      ggplot2::ylim(min(-2.5, dplyr::pull(data, {{ fold_change }})) - 0.5, max(2.5, dplyr::pull(data, {{ fold_change }})) + 0.5) +
+      ggplot2::scale_x_continuous(limits = NULL, expand = c(0, 0)) +
+      ggplot2::labs(
+        x = "Protein Sequence",
+        y = "log2(fold change)",
+        title = {
+          if (!missing(protein_id)) unique(dplyr::pull(data, {{ protein_id }}))
+        }
+      ) +
+      {
+        if (!missing(facet)) ggplot2::facet_wrap(rlang::new_formula(NULL, rlang::enquo(facet)), scales = "free", ncol = 4)
+      } +
+      ggplot2::guides(size = FALSE) +
+      {
+        if (!missing(colouring) && !is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_manual(values = protti_colours)
+      } +
+      {
+        if (!missing(colouring) && is.numeric(dplyr::pull(data, {{ colouring }}))) ggplot2::scale_fill_gradient(low = protti_colours[1], high = protti_colours[2])
+      } +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(
+          size = 20
+        ),
+        axis.text.x = ggplot2::element_text(
+          size = 15
+        ),
+        axis.text.y = ggplot2::element_text(
+          size = 15
+        ),
+        axis.title.y = ggplot2::element_text(
+          size = 15
+        ),
+        axis.title.x = ggplot2::element_text(
+          size = 15
+        ),
+        legend.title = ggplot2::element_text(
+          size = 15
+        ),
+        legend.text = ggplot2::element_text(
+          size = 15
+        ),
+        strip.text.x = ggplot2::element_text(
+          size = 15
+        ),
+        strip.text = ggplot2::element_text(
+          size = 15
+        ),
+        strip.background = element_blank()
+      )
+  }
 }
