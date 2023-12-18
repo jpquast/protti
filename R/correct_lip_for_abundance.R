51ec9b0f35d618848afec9481b744c7dd8ab3622
Elena Krismer <70535771+elena-krismer@users.noreply.github.com>
2023-12-18 18:36:22 +0100

    style file

--- R/correct_lip_for_abundance.R
+++ R/correct_lip_for_abundance.R
@@ -50,67 +50,73 @@
 #'
 #' # Load example data and simulate tryptic data by summing up precursors
 #'
-#' data = rapamycin_10uM
+#' data <- rapamycin_10uM
 #'
-#' data_trp = data %>%
-#' dplyr::group_by(pg_protein_accessions, r_file_name) %>%
-#' dplyr::mutate(pg_quantity = sum(fg_quantity)) %>%
-#' dplyr::distinct(r_condition,
-#'               r_file_name,
-#'               pg_protein_accessions,
-#'               pg_quantity)
+#' data_trp <- data %>%
+#'   dplyr::group_by(pg_protein_accessions, r_file_name) %>%
+#'   dplyr::mutate(pg_quantity = sum(fg_quantity)) %>%
+#'   dplyr::distinct(
+#'     r_condition,
+#'     r_file_name,
+#'     pg_protein_accessions,
+#'     pg_quantity
+#'   )
 #'
 #'
 #' # Calculate differential abundances for LiP and Trp data
 #'
+#' diff_lip <- data %>%
+#'   dplyr::mutate(fg_intensity_log2 = log2(fg_quantity)) %>%
+#'   assign_missingness(
+#'     sample = r_file_name,
+#'     condition = r_condition,
+#'     intensity = fg_intensity_log2,
+#'     grouping = eg_precursor_id,
+#'     ref_condition = "control",
+#'     retain_columns = "pg_protein_accessions"
+#'   ) %>%
+#'   calculate_diff_abundance(
+#'     sample = r_file_name,
+#'     condition = r_condition,
+#'     grouping = eg_precursor_id,
+#'     intensity_log2 = fg_intensity_log2,
+#'     comparison = comparison,
+#'     method = "t-test",
+#'     retain_columns = "pg_protein_accessions"
+#'   )
 #'
-#' diff_lip = data %>%
-#'     mutate(fg_intensity_log2 = log2(fg_quantity)) %>%
-#'     assign_missingness(
-#'       sample = r_file_name,
-#'                    condition = r_condition,
-#'                    intensity = fg_intensity_log2,
-#'                    grouping = eg_precursor_id,
-#'                    ref_condition = "control",
-#'                    retain_columns = "pg_protein_accessions") %>%
-#'     calculate_diff_abundance(sample = r_file_name,
-#'                    condition = r_condition,
-#'                    grouping = eg_precursor_id,
-#'                    intensity_log2 = fg_intensity_log2,
-#'                    comparison = comparison,
-#'                    method = "t-test",
-#'                    retain_columns = "pg_protein_accessions")
 #'
-#'
-#' diff_trp = data_trp %>%
-#'      mutate(pg_intensity_log2 = log2(pg_quantity)) %>%
-#'      assign_missingness(sample = r_file_name,
-#'                    condition = r_condition,
-#'                    intensity = pg_intensity_log2,
-#'                    grouping = pg_protein_accessions,
-#'                    ref_condition = "control") %>%
-#'      calculate_diff_abundance(sample = r_file_name,
-#'                    condition = r_condition,
-#'                    grouping = pg_protein_accessions,
-#'                    intensity_log2 = pg_intensity_log2,
-#'                    comparison = comparison,
-#'                    method = "t-test")
+#' diff_trp <- data_trp %>%
+#'   dplyr::mutate(pg_intensity_log2 = log2(pg_quantity)) %>%
+#'   assign_missingness(
+#'     sample = r_file_name,
+#'     condition = r_condition,
+#'     intensity = pg_intensity_log2,
+#'     grouping = pg_protein_accessions,
+#'     ref_condition = "control"
+#'   ) %>%
+#'   calculate_diff_abundance(
+#'     sample = r_file_name,
+#'     condition = r_condition,
+#'     grouping = pg_protein_accessions,
+#'     intensity_log2 = pg_intensity_log2,
+#'     comparison = comparison,
+#'     method = "t-test"
+#'   )
 #'
 #' # Correct for abundance changes
 #'
-#' corrected = correct_lip_for_abundance(
-#'
+#' corrected <- correct_lip_for_abundance(
 #'   lip_data = diff_lip,
 #'   trp_data = diff_trp,
 #'   protein_id = pg_protein_accessions,
 #'   grouping = eg_precursor_id,
 #'   retain_columns = c("missingness"),
-#'   method = "satterthwaite")
+#'   method = "satterthwaite"
+#' )
 #'
 #' head(corrected, n = 10)
-
-
-correct_lip_for_abundance = function(
+correct_lip_for_abundance <- function(
     lip_data,
     trp_data,
     protein_id,
@@ -121,93 +127,94 @@ correct_lip_for_abundance = function(
     std_error = std_error,
     p_adj_method = "BH",
     retain_columns = NULL,
-    method = c("satterthwaite","no_df_approximation")){
+    method = c("satterthwaite", "no_df_approximation")) {
+  method <- match.arg(method)
 
-  method = match.arg(method)
+  se_pep <- rlang::sym(paste0(rlang::as_name(rlang::enquo(std_error)), "_pep"))
+  se_prot <- rlang::sym(paste0(rlang::as_name(rlang::enquo(std_error)), "_prot"))
+  diff_pep <- rlang::sym(paste0(rlang::as_name(rlang::enquo(diff)), "_pep"))
+  diff_prot <- rlang::sym(paste0(rlang::as_name(rlang::enquo(diff)), "_prot"))
+  n_pep <- rlang::sym(paste0(rlang::as_name(rlang::enquo(n_obs)), "_pep"))
+  n_prot <- rlang::sym(paste0(rlang::as_name(rlang::enquo(n_obs)), "_prot"))
 
-  se_pep =  rlang::sym(paste0(rlang::as_name(rlang::enquo(std_error)),"_pep"))
-  se_prot = rlang::sym(paste0(rlang::as_name(rlang::enquo(std_error)),"_prot"))
-  diff_pep = rlang::sym(paste0(rlang::as_name(rlang::enquo(diff)),"_pep"))
-  diff_prot = rlang::sym(paste0(rlang::as_name(rlang::enquo(diff)),"_prot"))
-  n_pep = rlang::sym(paste0(rlang::as_name(rlang::enquo(n_obs)),"_pep"))
-  n_prot = rlang::sym(paste0(rlang::as_name(rlang::enquo(n_obs)),"_prot"))
-
-  temp_lip_data = lip_data %>%
-    dplyr::select(!!enquo(retain_columns), {{comparison}}, {{ protein_id }}, {{ grouping}}, {{ diff }}, {{ n_obs }}, {{ std_error }}) %>%
+  temp_lip_data <- lip_data %>%
+    dplyr::select(!!enquo(retain_columns), {{ comparison }}, {{ protein_id }}, {{ grouping }}, {{ diff }}, {{ n_obs }}, {{ std_error }}) %>%
     dplyr::distinct()
 
-  temp_trp_data = trp_data %>%
-    dplyr::distinct({{comparison}}, {{ protein_id }}, {{ diff }}, {{ n_obs }}, {{ std_error }})
+  temp_trp_data <- trp_data %>%
+    dplyr::distinct({{ comparison }}, {{ protein_id }}, {{ diff }}, {{ n_obs }}, {{ std_error }})
 
 
-  test = temp_lip_data %>%
-    dplyr::distinct({{comparison}}, {{ protein_id }}, {{ grouping}})
+  test <- temp_lip_data %>%
+    dplyr::distinct({{ comparison }}, {{ protein_id }}, {{ grouping }})
 
-  if (nrow(test) != nrow(temp_lip_data)){
+  if (nrow(test) != nrow(temp_lip_data)) {
     message("Warning: Your data frame contains dublicated values due to retained columns. This will affect the multiple testing correction.")
-    }
+  }
 
-  combined_data = dplyr::left_join(
+  combined_data <- dplyr::left_join(
     x = temp_lip_data,
     y = temp_trp_data,
     by = c(rlang::as_name(rlang::enquo(comparison)), rlang::as_name(rlang::enquo(protein_id))),
     suffix = c("_pep", "_prot")
   )
 
-  n_unmatched = combined_data %>%
+  n_unmatched <- combined_data %>%
     dplyr::filter(is.na(!!diff_prot) == T) %>%
     nrow()
 
-  percent_unmatched = round(n_unmatched/nrow(combined_data)*100, 2)
+  percent_unmatched <- round(n_unmatched / nrow(combined_data) * 100, 2)
 
-  message(paste0("No protein data was available for ",n_unmatched," peptides (",percent_unmatched, "% of dataset)."))
-
-  if (method == "satterthwaite"){
-
-  corrected_data = combined_data %>%
-    dplyr::mutate(adj_diff = !!diff_pep - !!diff_prot) %>%
-    dplyr::mutate(adj_std_error = sqrt( (!!se_pep)**2 + (!!se_prot)**2) ) %>%
-    dplyr::mutate(numer = ((!!se_pep)**2 + (!!se_prot)**2)**2) %>%
-    dplyr::mutate(denom = ( (!!se_pep)**4 / (!!n_pep-2) + (!!se_prot)**4/(!!n_prot-2))) %>%
-    dplyr::mutate(df = numer/denom) %>%
-    dplyr::mutate(tval = adj_diff / adj_std_error) %>%
-    dplyr::mutate(pval = 2*stats::pt(abs(tval), df, lower.tail = FALSE))
-
-
-  adjusted_data = dplyr::left_join(x = corrected_data,
-                                   y = corrected_data %>%
-                                     dplyr::filter(is.na(pval) == FALSE) %>%
-                                     dplyr::group_by({{ comparison }}) %>%
-                                     dplyr::mutate(adj_pval = p.adjust(pval, method = {{ p_adj_method }})),
-                                   by = colnames(corrected_data)) %>%
-    dplyr::select(- numer, - denom, - tval)
-
-  return(adjusted_data)
-
+  if (n_unmatched != 0) {
+    message(paste0("No protein data was available for ", n_unmatched, " peptides (", percent_unmatched, "% of dataset)."))
   }
 
-  if (method == "no_df_approximation"){
 
-    corrected_data = combined_data %>%
+  if (method == "satterthwaite") {
+    corrected_data <- combined_data %>%
+      dplyr::mutate(adj_diff = !!diff_pep - !!diff_prot) %>%
+      dplyr::mutate(adj_std_error = sqrt((!!se_pep)**2 + (!!se_prot)**2)) %>%
+      dplyr::mutate(numer = ((!!se_pep)**2 + (!!se_prot)**2)**2) %>%
+      dplyr::mutate(denom = ((!!se_pep)**4 / (!!n_pep - 2) + (!!se_prot)**4 / (!!n_prot - 2))) %>%
+      dplyr::mutate(df = numer / denom) %>%
+      dplyr::mutate(tval = adj_diff / adj_std_error) %>%
+      dplyr::mutate(pval = 2 * stats::pt(abs(tval), df, lower.tail = FALSE))
+
+
+    adjusted_data <- dplyr::left_join(
+      x = corrected_data,
+      y = corrected_data %>%
+        dplyr::filter(is.na(pval) == FALSE) %>%
+        dplyr::group_by({{ comparison }}) %>%
+        dplyr::mutate(adj_pval = p.adjust(pval, method = {{ p_adj_method }})),
+      by = colnames(corrected_data)
+    ) %>%
+      dplyr::select(-numer, -denom, -tval)
+
+    return(adjusted_data)
+  }
+
+  if (method == "no_df_approximation") {
+    corrected_data <- combined_data %>%
       dplyr::mutate(
         adj_diff = !!diff_pep - !!diff_prot,
-        adj_std_error = sqrt( (!!se_pep)**2 + (!!se_prot)**2),
+        adj_std_error = sqrt((!!se_pep)**2 + (!!se_prot)**2),
         df = !!n_pep - 2,
         tval = adj_diff / adj_std_error,
-        pval = 2*stats::pt(abs(tval), df, lower.tail = FALSE)
+        pval = 2 * stats::pt(abs(tval), df, lower.tail = FALSE)
       )
 
 
-    adjusted_data = dplyr::left_join(x = corrected_data,
-                                     y = corrected_data %>%
-                                       dplyr::filter(is.na(pval) == FALSE) %>%
-                                       dplyr::mutate(adj_pval = p.adjust(pval, method = {{ p_adj_method }})),
-                                     by = colnames(corrected_data)) %>%
-      dplyr::select(- tval)
+    adjusted_data <- dplyr::left_join(
+      x = corrected_data,
+      y = corrected_data %>%
+        dplyr::filter(is.na(pval) == FALSE) %>%
+        dplyr::mutate(adj_pval = p.adjust(pval, method = {{ p_adj_method }})),
+      by = colnames(corrected_data)
+    ) %>%
+      dplyr::select(-tval)
 
 
     return(adjusted_data)
-
   }
-
 }
